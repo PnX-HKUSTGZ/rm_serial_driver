e3e19af7aa4de59471c4db6629545abcf7d77a12
cytochronne <yxie667@connect.hkust-gz.edu.cn>
2025-03-17 13:04:46 +0800

    [update]fix the quternion and yawpitch position

--- src/rm_serial_driver.cpp
+++ src/rm_serial_driver.cpp
@@ -137,10 +137,11 @@ void RMSerialDriver::receiveData()
           tf2::Quaternion q_rot;
           //double PI = 3.1415926;
           q_rot.setRPY(0, 0, 0);
-          tf2::Quaternion q(packet.q[0] ,packet.q[1], packet.q[2], packet.q[3]);
+          tf2::Quaternion q(packet.q[1], packet.q[2], packet.q[3], packet.q[0]);
+          //std::cout<<packet.q[0]<<" "<<packet.q[1]<<" "<<packet.q[2]<<" "<<packet.q[3]<<std::endl;
           double roll, pitch, yaw; 
           tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
-          q.setRPY(pitch, -roll, yaw);
+          q.setRPY(-pitch, roll, yaw);
         
           q_rot = q * q_rot;
           t.transform.rotation = tf2::toMsg(q_rot);
